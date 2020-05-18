97c9625a7ff73550943008a76eb0056c49509f33
Paul Fjare <pfjare@gmail.com>
2020-05-18 11:44:23 -0600

    finish

--- projects/03_coffee_shop_full_stack/starter_code/backend/README.md
+++ projects/03_coffee_shop_full_stack/starter_code/backend/README.md
@@ -70,8 +70,8 @@ The `--reload` flag will detect file changes and restart the server automaticall
    - Manager
      - can perform all actions
 7. Test your endpoints with [Postman](https://getpostman.com). - Register 2 users - assign the Barista role to one and Manager role to the other. - Sign into each account and make note of the JWT. - Import the postman collection `./starter_code/backend/udacity-fsnd-udaspicelatte.postman_collection.json` - Right-clicking the collection folder for barista and manager, navigate to the authorization tab, and including the JWT in the token field (you should have noted these JWTs). - Run the collection and correct any errors. - Export the collection overwriting the one we've included so that we have your proper JWTs during review!
-   https://classudacity.auth0.com/authorize?audience=coffee&response_type=token&client_id=0p05fRWKlFcByjn0Jx2G1qmj0ioiglgI&redirect_uri=https://127.0.0.1:8080/
-   pfjare@gmail.com Grapes20
+
+https://classudacity.auth0.com/authorize?audience=coffee&response_type=token&client_id=0p05fRWKlFcByjn0Jx2G1qmj0ioiglgI&redirect_uri=https://127.0.0.1:8080/
 
 ### Implement The Server
 
--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -15,51 +15,41 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+#db_drop_and_create_all()
 
-## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+# ROUTES
 
+# GET /drinks (Public)
 @app.route('/drinks', methods=['GET'])
 def get_drinks():
     drinks = Drink.query.all()
+
     return jsonify({
         "success": True,
         "drinks": [drink.short() for drink in drinks]
         })
 
+
+# GET /drinks - Requires get:drinks-detail permission
 @app.route('/drinks-detail', methods=['GET'])
 @requires_auth('get:drinks-detail')
 def get_drinks_detail():
     drinks = Drink.query.all()
+
     return jsonify({
         "success": True,
         "drinks": [drink.long() for drink in drinks]
     })
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 
+# POST /drinks - Requires post:drinks permission
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
 def post_drinks():
     data = request.get_json()
     title = data.get('title', None)
     recipe = data.get('recipe', None)
-
+    # Request body must contain both a title and recipe
     if recipe is None or title is None:
         abort(422)
     try:
@@ -71,47 +61,26 @@ def post_drinks():
 
     return jsonify({
         "success": True,
-        "drinks": drink.long()
+        "drinks": [drink.long()]
         })
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 
-
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
-
+#PATCH /drinks/:id - Requires patch:drinks permission
 @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
 @requires_auth('patch:drinks')
 def update_drinks(drink_id):
-    
+
     data = request.get_json()
     title = data.get('title', None)
     recipe = data.get('recipe', None)
+    # Request body must contain either a title or a recipe or both
     if recipe is None and title is None:
         abort(422)
-   
+
     drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
-    print(drink)
     if drink is None:
         abort(404)
-   
+
     try:
         if title:
             drink.title = title
@@ -122,40 +91,31 @@ def update_drinks(drink_id):
     except exc.SQLAlchemyError as error:
         print(error)
         abort(422)
-    
+
     return jsonify({
         "success": True,
-        "drinks": drink.long()
+        "drinks": [drink.long()]
     })
 
+
+# DELETE /drinks/:id - Requires delete:drinks permission
 @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drinks(drink_id):
     drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
     if drink is None:
         abort(404)
-    
+
     try:
         drink.delete()
-
     except exc.SQLAlchemyError as error:
         print(error)
         abort(422)
+
     return jsonify({
         "success": True,
         "delete": drink.id
     })
-    
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
 
 
 # Error Handling
@@ -176,8 +136,7 @@ def resource_not_found(error):
         "message": "resource not found"
         }), 404
 
+#AuthError defined in auth.py
 @app.errorhandler(AuthError)
 def authentication(error):
-    return jsonify(error.error), error.status_code
-
-
+    return jsonify(error.error), 401
